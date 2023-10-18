18ff94c895854f3232841159ad49051f9591d922
Tobias Sirne <sirnetobias1@gmail.com>
2023-10-17 21:10:30 -0300

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FillingComponent } from './filling/filling.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+import { WritingComponent } from './writing/writing.component';
+import { ContactComponent } from './contact/contact.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FillingComponent,
+    FooterComponent,
+    AboutComponent,
+    WorkComponent,
+    WritingComponent,
+    ContactComponent,
+    InicioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
